9ce997abbce911426bd67695f535c0cb7942877d
Hideaki Kusaka <gsf.zero1@devlights.onmicrosoft.com>
2013-08-30 13:22:42 +0900

    using部分をnamespaceの下に移動

--- Retry/Devlights.Samples.Retry.Tests/RetryHandlerTest.cs
+++ Retry/Devlights.Samples.Retry.Tests/RetryHandlerTest.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Devlights.Samples.Retry
+﻿namespace Devlights.Samples.Retry
 {
+  using System;
+  using System.Collections.Generic;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  
   [TestClass]
   public class RetryHandlerTest
   {
